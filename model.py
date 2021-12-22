5bee65b13a596698e38f6f09fc54a368fc1134dd
smal1378 <smal137800@gmail.com>
2021-12-22 16:59:14 +0330

    Write model.py, 'Core' and 'Gens' classes.

---
+++ model.py
@@ -0,0 +1,36 @@
+
+class Gens:
+    def __init__(self, name, _id, price, info):
+        self.name = name
+        self.id = _id
+        self.price = price
+        self.info = info
+        self.amount = 0
+
+    def edit(self, name=None, price=None, info=None):
+        if name is not None:
+            self.name = name
+        if price is not None:
+            self.price = price
+        if info is not None:
+            self.info = info
+
+    def sell(self, amount):
+        self.amount -= amount
+
+    def buy(self, amount):
+        self.amount += amount
+
+
+class Core:
+    def __init__(self):
+        self.genses = []
+        self.last_gens_id = 0
+
+    def add_gens(self, name, price, info):
+        self.last_gens_id += 1
+        new_gens = Gens(name, self.last_gens_id, price, info)
+        self.genses.append(new_gens)
+
+    def get_gens(self):
+        return self.genses
